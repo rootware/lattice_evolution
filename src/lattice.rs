4cb95c17cf72ae04d411f6b48423f1f1f2d829e7
rootware <Shah.Saad.Alam@rice.edu>
2024-01-13 00:25:19 -0700

    documentation

--- src/lattice.rs
+++ src/lattice.rs
@@ -5,35 +5,40 @@ use std::f64::consts::PI;
 const MASS: f64 = 1.0;
 const N_STEPS : i128 = 10000;
 
+/// Lattice struct represents an instance of the Shaken Optical Lattice
 #[derive(Debug)]
 pub struct Lattice {
-    g : f64,
-    q: f64,
-    depth: f64,
-    h0: DMatrix<Complex64>,
-    h1: DMatrix<Complex64>,
-    h2: DMatrix<Complex64>,
-    psi: DVector<Complex64>
+    g : f64, // acceleration/gravity
+    q: f64, // Correction to pure momentum, $(p+q)$ is kinematic momentum
+    depth: f64, // lattice depth
+    h0: DMatrix<Complex64>, //The Kinetic part of the Hamiltonian
+    h1: DMatrix<Complex64>, //The $sin(\phi)$ part of the lattice coupling
+    h2: DMatrix<Complex64>, // the $\cos(\phi)$ part of the lattice coupling
+    psi: DVector<Complex64> // current wavefunction of wavepacket moving through lattice
 }
 
 impl Lattice {
+    /// Returns $V_0$ of this Lattice
     pub fn get_depth(&self) -> f64 {
         self.depth
     }
-
+    /// Sets $V_0$ of this Lattice
     pub fn set_depth(&mut self, depth: f64) {
         self.depth = depth;
     }
 
+    /// Returns full Hamiltonian with $\phi=0$ 
     pub fn get_hamiltonian(&self) -> DMatrix<Complex64> {
         let hamiltonian = &self.h0 - &self.h2;
         hamiltonian
     }
 
+    /// Current Wavefunction of the wavepacket
     pub fn get_psi(&self) -> DVector<Complex64> {
         self.psi.clone()
     }
 
+    /// Accelerate the wavepacket by adding an _impulse_ to $q$ and updating kinematic momentum
     pub fn accelerate(&mut self, impulse: f64) {
         self.q += impulse;
         let max_p: f64 = (N_STATES as f64)-1.0 ; 
@@ -47,6 +52,12 @@ impl Lattice {
         self.h0 = h0new;
     }
 
+    /// Evolve wavepacket in this lattice given $(A,\omega)$ for $\phi=A\sin(\omega t)$
+    /// for a time period $period = \pi/(11.5 \omega)$.
+    /// Usually, we set $\omega=11.5\omega_r$ to be constant, given by FREQ in Lattice module
+    /// 
+    /// the step() method calls the rk4step() solver, which uses Runge-Kutta 4 to update the system. 
+    /// To do so it calls update(), the method for calculating derivatives
     pub fn step( &mut self, amplitude: f64, omega: f64){
         let mut time : f64 = 0.0;
         let period : f64 = PI/omega;    
@@ -60,12 +71,15 @@ impl Lattice {
         
     }
 
+    /// update is essentially the derivative to the wavepacket at time t$
+    /// It's given by $-i*H*|\psi>$
     pub fn update(&self, wavefunction: DVector<Complex64>, amplitude:f64, omega: f64, t: f64 ) -> DVector<Complex64> {
         let phi = amplitude*f64::sin(omega*t);
         let hamiltonian = &self.h0 + &self.h1*Complex64::from(f64::sin(phi)) - &self.h2*Complex64::from(f64::cos(phi));
         hamiltonian*wavefunction*Complex64::new(0.0,-1.0)
     }
 
+    /// The RK4 step.
     pub fn rk4step(&mut self, dt:f64, amplitude: f64, omega: f64, t:f64){
         let k1 = self.update(self.psi.clone(), amplitude, omega, t);
         let k2 = self.update(self.psi.clone() + &k1*Complex64::from(dt/2.0) , amplitude, omega, t+dt/2.0);
@@ -79,6 +93,12 @@ impl Lattice {
 
 impl Lattice {
 
+    /// Initializes the Lattice and makes a new instnce
+    /// Uses given values of $(a,V_0)$ (see paper) to construct the Hamiltonian.
+    /// It further initializes the wavepacket to be the ground state.
+    /// 
+    /// By default, the const N_STATES defines how many momentum states are included in the
+    /// simulation basis. So N_STATES=11 means $p\in \{-10\hbark_L, -8\hbar k_L, ..., 10\hbar k_L\}$
     pub fn new(acceleration: f64, latticedepth: f64) -> Lattice {
         let max_p: f64 = (N_STATES as f64)-1.0 ; 
 
@@ -109,8 +129,8 @@ impl Lattice {
         if psitemp[5].re <= 0.0 {
             psitemp = &psitemp*Complex64::from(-1.0);
         };
-      //  let mut psitemp :DVector<Complex64> = eigvec.column(3).into();
-      //  psitemp = &psitemp/Complex64::from((&psitemp).norm_squared());
+
+        
         Self {
             g: acceleration,
             q: 0.0,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
- pub mod lattice;// use std::f64::consts::PI;
-// use ndarray::Zip;
+pub mod lattice;
 use num_complex::Complex64;
 use lattice::Lattice;
 use rayon::prelude::*;
@@ -10,33 +9,45 @@ use std::fs::File;
 use std::io::Write;
 use std::sync::Mutex;
 
+/// This const is usually used to fix $\omega$ for our shaking functions 
+/// to be $\omega=11.5\omega_r$, since we only vary the amplitude of the shaking function in this RL.
 const FREQ: f64 = 11.5;
+
+/// Decide if we first shake to the +ve or -ve x direction.
+/// Each subsequent shaking has opposite sign.
 const TOGGLE_INIT : f64 = 1.0;
 
 
-
+/// #Description of code:
+/// 
+/// AUTHOR: SHAH SAAD ALAM
+/// 
+/// main method takes in a hard-coded shaking sequence, multi-thread iterates over acceleration values and writes out in _test.txt_ 
+/// the output. output is in the form for different $p$ values as determined by N_STATES
+/// 
+/// [acc index, lattice index, acceleration $a$ , lattice depth $V_0$ , P(p|a,V_0$ ]
 fn main() {
-  /*/ let shakingfunctions2p2e: Vec<f64>= vec![1.83259571, 0., 1.83259571, 2.87979327, 1.83259571, 1.83259571, 1.83259571, 3.40339204, 3.66519143,
-   3.40339204, 3.40339204, 3.14159265, 3.92699082, 3.92699082, 2.35619449, 2.35619449, 3.92699082, 3.92699082,
-   3.92699082, 3.66519143, 3.66519143, 3.66519143, 2.61799388, 3.66519143, 1.57079633, 1.57079633, 1.57079633,
-   1.04719755, 1.04719755, 1.04719755, 1.04719755, 1.57079633]; */
 
+   // hard code our shaking sequence
    let latt_shaking : Vec<f64> = vec![1.83259571, 0., 1.83259571, 2.87979327, 1.83259571, 1.83259571, 1.83259571, 3.40339204, 3.66519143,
    3.40339204, 3.40339204, 3.14159265, 3.92699082, 3.92699082, 2.35619449, 2.35619449, 3.92699082, 3.92699082,
    3.92699082, 3.66519143, 3.66519143, 3.66519143, 2.61799388, 3.66519143, 1.57079633, 1.57079633, 1.57079633,
    1.04719755, 1.04719755, 1.04719755, 1.04719755, 1.57079633];
    
+   // Create file
    let _file2 = File::create("test.txt").unwrap();
-   //file2.write_all(b"Create\n").unwrap();
 
+   // Open file
    let file = OpenOptions::new()
       .write(true)
       .append(true)
       .open("test.txt").unwrap();
-   //file.write_all(b"Metadata\n").unwrap();
 
+   // Wrap file in Mutex for thread safety
    let file = Mutex::new(file);
 
+   // We multithread iterator over acceleration, but not lattice depth. 
+   // Sufficient for my laptop/desktop.
    let _sum : Vec<f64> = (0..101).into_par_iter().map(|x| {
       let acc = -0.00225 + (0.00225*2.0 * x as f64)/(100 as f64);
       
