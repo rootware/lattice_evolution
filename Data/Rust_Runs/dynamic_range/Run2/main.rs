f8c5d6475cc29dbcdb931e7bf197fc2128a6c051
rootware <Shah.Saad.Alam@rice.edu>
2024-02-10 12:32:46 -0700

    diagnose 4.0 cfi factor

--- Data/Rust_Runs/dynamic_range/Run2/main.rs
+++ Data/Rust_Runs/dynamic_range/Run2/main.rs
@@ -1,9 +1,8 @@
 pub mod lattice;
-use nalgebra::DVector;
 use num_complex::Complex64;
 use lattice::Lattice;
 use rayon::prelude::*;
-use std::f64::consts::PI;
+
 
 use std::fs::OpenOptions;
 use std::fs::File;
@@ -38,30 +37,31 @@ fn main() {
    3.40339204, 3.40339204, 3.14159265, 3.92699082, 3.92699082, 2.35619449, 2.35619449, 3.92699082, 3.92699082,
    3.92699082, 3.66519143, 3.66519143, 3.66519143, 2.61799388, 3.66519143, 1.57079633, 1.57079633, 1.57079633,
    1.04719755, 1.04719755, 1.04719755, 1.04719755, 1.57079633];//1param acc
+   
    // Create file
-   let _file2 = File::create("./testing_cfi/test.txt").unwrap();
+   let _file2 = File::create("./dynamic_range/test.txt").unwrap();
 
    // Open file
    let file = OpenOptions::new()
       .write(true)
       .append(true)
-      .open("./testing_cfi/test.txt").unwrap();
+      .open("./dynamic_range/test.txt").unwrap();
 
    // Wrap file in Mutex for thread safety
    let file = Mutex::new(file);
 
    // We multithread iterator over acceleration, but not lattice depth. 
    // Sufficient for my laptop/desktop.
-   println!("Testing CFI calculations using the augmented state method");
+   println!("Generating multiparam Bayesian priors for dynamic range testing sequence");
 
-   let bar = ProgressBar::new(100 );
+   let bar = ProgressBar::new(1000 );
    bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] {wide_bar:100.cyan/blue} {pos:>7}/{len:7} {msg}")
     .unwrap()
     .progress_chars("##-"));
 
-   let _sum : Vec<f64> = (0..101).into_par_iter().map(|x| {
+   let _sum : Vec<f64> = (0..1001).into_par_iter().map(|x| {
      // let acc = -0.00225 + (0.00225*2.0 * x as f64)/(1000 as f64);
-     let acc = - 0.0225 + ( 0.0225*2.0 * x as f64)/(100 as f64);
+     let acc = - 0.2 + ( 0.2*2.0 * x as f64)/(1000 as f64);
      for y in 0..1 {
         // let latdep : f64 =  9.0 + (2.0* y as f64)/(50 as f64);
         let latdep : f64 =  10.0;
@@ -76,15 +76,15 @@ fn main() {
              sign *= -1.0;
          };
 
-         let tof = PI/11.5 *32.0;
-         let f_mz = ( 4.0 * 2.0 * tof.powi(2) ).powi(2)/4.0;
-         let result = vec![latt.acc_cfi() / f_mz *4.0];
+         let out = latt.get_psi();
+         let momentum_i: Vec<Complex64> = (out.conjugate().component_mul(&out)).data.into();
+         let momentum : Vec<f64> = momentum_i.iter().map(|&m| m.re).collect();
          let mut s = String::new();
          s =  s + &format!("{x}\t{y}\t");
          s =  s + &format!("{acc}\t{latdep}\t");
 
 
-         for num in result {
+         for num in momentum {
             s.push_str(&num.to_string());
             s.push_str("\t");
          }
@@ -100,19 +100,6 @@ fn main() {
       
       bar.inc(1); acc}).collect();
 
-
-      // test my cfi module
-      let dPsi = DVector::from_vec( vec! [ Complex64::new( 0.5, 0.0), Complex64::new(0.2,0.3)]) ;
-      let psi = DVector::from_vec( vec! [ Complex64::new( 0.9, 0.1), Complex64::new(0.2,0.3)]) ;
-
-      let p_a : f64 = dPsi.iter().zip(psi.iter()).map(|(&dp, &p)|
-      { (2.0*(dp*p.conj()).re ).powi(2)/( p.norm_sqr())}
-      ).collect::<Vec<f64>>().iter().sum();
-
-      println!( "{p_a}");
-
-      let mut byhand = (2.0*( 0.5*0.9))*(2.0*( 0.5*0.9))/(0.81+0.01) + (2.0*( 0.2*0.2 + 0.3*0.3))*(2.0*( 0.2*0.2 + 0.3*0.3)) /(0.04+0.09);
-      println!("{byhand}");
 
    
 }
--- src/lattice_original.rs
+++ src/lattice_original.rs
@@ -1,5 +1,5 @@
 use num_complex::Complex64;
-use nalgebra::{   DMatrix, DVector};
+use nalgebra::{ DMatrix,DVector};
 const N_STATES : usize = 11; // Size of matrices
 use std::f64::consts::PI;
 const MASS: f64 = 1.0;
@@ -14,8 +14,6 @@ pub struct Lattice {
     q: f64, 
     /// lattice depth
     depth: f64, 
-    /// time
-    time: f64,
     ///The Kinetic part of the Hamiltonian
     h0: DMatrix<Complex64>, 
     ///The $sin(\phi)$ part of the lattice coupling
@@ -23,16 +21,10 @@ pub struct Lattice {
     /// the $\cos(\phi)$ part of the lattice coupling
     h2: DMatrix<Complex64>, 
     /// current wavefunction of wavepacket moving through lattice
-    psi: DVector<Complex64> ,
-    dpsi_a: DVector<Complex64>
+    psi: DVector<Complex64> 
 }
 
 impl Lattice {
-
-    /// Returns $a$ of this Lattice
-    pub fn get_acceleration(&self) -> f64 {
-        self.g
-    }
     /// Returns $V_0$ of this Lattice
     pub fn get_depth(&self) -> f64 {
         self.depth
@@ -52,29 +44,6 @@ impl Lattice {
     pub fn get_psi(&self) -> DVector<Complex64> {
         self.psi.clone()
     }
-
-    pub fn get_time(&self) -> f64 {
-        self.time
-    }
-    pub fn get_dpsi_a(&self) -> DVector<Complex64>{
-        self.dpsi_a.clone()
-    }
-
-    pub fn get_momentum(&self) -> DVector<f64> {
-        let out = self.get_psi();
-        let momentum_i = out.conjugate().component_mul(&out);
-        let momentum  = DVector::from_vec(momentum_i.into_iter().map(|&m| m.re).collect());
-        momentum
-    }
-
-    pub fn get_dmomentum_da(&self) -> DVector<f64> {
-        let out = self.get_dpsi_a();
-        let dmomentum_i = out.conjugate().component_mul(&out);
-        let dmomentum  = DVector::from_vec(dmomentum_i.into_iter().map(|&m| m.re).collect());
-        dmomentum
-    }
-
-
 
     /// Accelerate the wavepacket by adding an _impulse_ to $q$ and updating kinematic momentum
     pub fn accelerate(&mut self, impulse: f64) {
@@ -106,7 +75,6 @@ impl Lattice {
             self.rk4step( dt,  amplitude, omega, time);
             it+=1; time+=dt;
         }
-        self.time+= time;
         
     }
 
@@ -118,25 +86,6 @@ impl Lattice {
         hamiltonian*wavefunction*Complex64::new(0.0,-1.0)
     }
 
-    /// update_d is essentially the derivative to the state derivative at time t$
-    /// It's given by $-i*H*|\psi> +i*\hat{p}t$
-    pub fn update_d(&self, state_deriv: DVector<Complex64>, state: DVector<Complex64>,amplitude:f64, omega: f64, t: f64 ) -> DVector<Complex64> {
-        let phi = amplitude*f64::sin(omega*t);
-        let hamiltonian = &self.h0 + &self.h1*Complex64::from(f64::sin(phi)) - &self.h2*Complex64::from(f64::cos(phi));
-
-
-        let max_p: f64 = (N_STATES as f64)-1.0 ; 
-
-        let diagonal = DVector::<Complex64>::from_vec( (0..N_STATES).map(|i| {
-            Complex64::new(i as f64 * 2.0 -max_p, 0.0)//remove self.q
-        }).collect() );
-        // Is there an easier way to convert the f64 diagonal into a Complex valued array?
-
-
-        hamiltonian*state_deriv*Complex64::new(0.0,-1.0) + diagonal.component_mul(&state)*Complex64::new( 0.0, self.time +t)
-    }
-
-    
     /// The RK4 step.
     pub fn rk4step(&mut self, dt:f64, amplitude: f64, omega: f64, t:f64){
         let k1 = self.update(self.psi.clone(), amplitude, omega, t);
@@ -144,14 +93,7 @@ impl Lattice {
         let k3 = self.update(self.psi.clone() + &k2*Complex64::from(dt/2.0) , amplitude, omega, t+dt/2.0);
         let k4 = self.update(self.psi.clone() + &k3*Complex64::from(dt) , amplitude, omega, t+dt);
 
-        let k1_d = self.update_d(self.dpsi_a.clone(), self.psi.clone(), amplitude, omega, t);
-        let k2_d = self.update_d(self.dpsi_a.clone() + &k1_d*Complex64::from(dt/2.0) ,self.psi.clone()+ &k1*Complex64::from(dt/2.0), amplitude, omega, t+dt/2.0);
-        let k3_d = self.update_d(self.dpsi_a.clone() + &k2_d*Complex64::from(dt/2.0) , self.psi.clone()+&k2*Complex64::from(dt/2.0) ,amplitude, omega, t+dt/2.0);
-        let k4_d = self.update_d(self.dpsi_a.clone() + &k3_d*Complex64::from(dt) , self.psi.clone()+&k3*Complex64::from(dt),amplitude, omega, t+dt);
-
         self.psi = &self.psi + ( k1 + k2*Complex64::from(2.0) + k3*Complex64::from(2.0) + k4)*Complex64::from(dt/6.0);
-        self.dpsi_a = &self.dpsi_a + ( k1_d + k2_d*Complex64::from(2.0) + k3_d*Complex64::from(2.0) + k4_d)*Complex64::from(dt/6.0);
-
         self.accelerate(-MASS*self.g*dt);
     }
 }
@@ -200,34 +142,11 @@ impl Lattice {
             g: acceleration,
             q: 0.0,
             depth: latticedepth,
-            time: 0.0,
             h0 ,
             h1 ,
             h2 ,
-            psi: psitemp,
-            dpsi_a: DVector::from_element( N_STATES,Complex64::new(0.0,0.0))        }
+            psi: psitemp
+        }
     }
 }
 
-impl Lattice {
-
-    pub fn acc_qfi(&self)-> f64 {
-        let dpsi_a_c = self.dpsi_a.clone();
-        let psi_c = self.psi.clone();
-
-        4.0 * ( dpsi_a_c.conjugate().dot(&dpsi_a_c) - (dpsi_a_c.conjugate().dot(&psi_c) ).norm().powi(2)    ).re
-    }
-
-    pub fn acc_cfi(&self) -> f64 {
-        let dpsi_a_c = self.dpsi_a.clone();
-        let psi_c = self.psi.clone();
-
-        let p_a : f64 = dpsi_a_c.iter().zip(psi_c.iter()).map(|(&dp, &p)|
-                    { (2.0*(dp*p.conj()).re ).powi(2)/( p.norm_sqr() )}
-                    ).collect::<Vec<f64>>().iter().sum();
-
-        p_a
-
-
-    }
-}
